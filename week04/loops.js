e6c6a612320907d22a65ff52b5036c103d98048c
sjjunker <sandi.junker@outlook.com>
2024-03-18 11:41:21 -0500

    Week04 Learnign Avtivity 1

---
+++ week04/loops.js
@@ -0,0 +1,67 @@
+// loops.js
+myInfo = {
+    name: "Brother T",
+    photo: "images/photo.jpg",
+    favoriteFoods: ["Fettucini", "Steak", "Chicken", "Shrimp", "Baked Potato"],
+    hobbies: ["Reading", "Fishing", "Camping"],
+    placesLived: [
+      {
+        place: "Rexburg, ID",
+        length: "5 years",
+      },
+      {
+        place: "Ammon, ID",
+        length: "3 years",
+      },
+      {
+        place: "Sandy, UT",
+        length: "1 year",
+      },
+    ],
+  };
+
+  //Add faovrite foods to ul
+  //forEach
+  /*myInfo.favoriteFoods.forEach(function(food) {
+    let favoriteFood = document.createElement("li");
+    favoriteFood.textContent = food;
+    document.getElementById('favorite-foods').appendChild(favoriteFood);
+  });*/
+
+  //map -- favicon.ico issue
+ /* const favoriteFoodsList = 
+    myInfo.favoriteFoods.map(function(food) {
+        let favoriteFood = document.createElement("li");
+        favoriteFood.textContent = food;
+        return favoriteFood
+    });
+  document.getElementById('favorite-foods').innerHTML = favoriteFoodsList.join('');*/
+
+  const foodsElement = document.querySelector("#favorite-foods");
+  const placesElement = document.querySelector("#places-lived");
+  // requires a list, and a callback that will produce an html string for each item in the list
+  // returns a string of html
+  function generateListMarkup(list, templateCallback) {
+    const htmlList = list.map(templateCallback);
+    return htmlList.join("");
+  }
+  // requires an food string
+  // returns that string embedded in HTML markup
+  function foodsTemplate(food) {
+    return `<li>${food}</li>`;
+  }
+  
+  // requires an place string
+  // returns that string embedded in HTML markup
+  function placesTemplate(place) {
+    return `<dt>${place.place}</dt><dd>${place.length}</dd>`;
+  }
+  
+  foodsElement.innerHTML = generateListMarkup(
+    myInfo.favoriteFoods,
+    foodsTemplate
+  );
+  placesElement.innerHTML = generateListMarkup(
+    myInfo.placesLived,
+    placesTemplate
+  );
---
+++ week04/loops2.js
@@ -0,0 +1,48 @@
+const DAYS = 6;
+const LIMIT = 30;
+let studentReport = [11, 42, 33, 64, 29, 37, 44];
+
+//for loop
+console.log("For Loop");
+for (let i = 0; i <= studentReport.length; i++) {
+    if( studentReport[i] < 30) {
+        console.log(studentReport[i]);
+    } 
+}
+
+//while loop
+console.log("While Loop");
+let counter = 0;
+
+while(counter < studentReport.length) {
+    if( studentReport[counter] < 30) {
+        console.log(studentReport[counter]);
+    } 
+    counter++;
+}
+
+//forEach
+console.log("forEach");
+
+studentReport.forEach(function (number) {
+    if(number < 30) {
+        console.log(number);
+    } 
+});
+
+//for in
+console.log("For In");
+
+for(let number in studentReport) {
+    if( studentReport[number] < 30) {
+        console.log(studentReport[number]);
+    } 
+}
+
+//Days of the week
+console.log("Days of the Week");
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+for(let i = 0; i < daysOfTheWeek.length; i++) {
+    console.log(daysOfTheWeek[i]);
+}
